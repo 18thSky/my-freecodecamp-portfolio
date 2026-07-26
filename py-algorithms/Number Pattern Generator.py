489469a1c8f4fb4e1f72897040f3d631130ffe3e
18thSky <akashhajjarge@gmail.com>
2026-07-26 20:24:41 +0530

    Add new Python algorithm challenge

---
+++ py-algorithms/Number Pattern Generator.py
@@ -0,0 +1,10 @@
+def number_pattern(n):
+    if not isinstance(n,int):
+            return 'Argument must be an integer value.'
+    if n <= 0:
+            return 'Argument must be an integer greater than 0.'
+
+    result = []
+    for number in range(1,n+1):
+        result.append(str(number))
+    return " ".join(result)
