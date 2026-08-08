91cdd9df2cd7d8e0e1decbbc331faf28e4bb0839
18thSky <akashhajjarge@gmail.com>
2026-08-08 13:43:55 +0530

    Add new Python algorithm challenge

--- py-algorithms/The Syndicate Payroll.py
+++ py-algorithms/The Syndicate Payroll.py
@@ -4,20 +4,19 @@
 # and a secure database of their hourly rates. You need to calculate everyone's pay, 
 # format a clean audit log for the boss, and calculate the total amount of cash leaving the vault.
 
-"""
-1. Create a master function called audit_payroll with parameters
-2. Crew(members) and hours(hours worked) which are list and a dict called rate_db(members:hour worked)
-3. Add isinstance check for all 3 parameters if follow the type which we passing or else return an error
-4. Need to create 2 variables a. total_vault_cash which is a int and set to 0 
-5. An empty List audit_log
-6. using a For loop and zip()method we need to iterate through every crew and hours entries at same time
-7. Create a new variable to save .lower text of hackers to clean_name
-8. Using Get list method we need to look up name from rate_db list and if its not present in DB return 100
-9. To Calculate payout we need to do math, hacker's hours * rate = total_vault_cash
-10. we need to return a formatted string return f""{hacker_name}.capitalize()": "{rate}""
-11. Also we need to add  get this some how {"total_paid": total_vault_cash, "log": audit_log}
+# 1. Create a master function called audit_payroll with parameters
+# 2. Crew(members) and hours(hours worked) which are list and a dict called rate_db(members:hour worked)
+# 3. Add isinstance check for all 3 parameters if follow the type which we passing or else return an error
+# 4. Need to create 2 variables a. total_vault_cash which is a int and set to 0 
+# 5. An empty List audit_log
+# 6. using a For loop and zip()method we need to iterate through every crew and hours entries at same time
+# 7. Create a new variable to save .lower text of hackers to clean_name
+# 8. Using Get list method we need to look up name from rate_db list and if its not present in DB return 100
+# 9. To Calculate payout we need to do math, hacker's hours * rate = total_vault_cash
+# 10. we need to return a formatted string return f""{hacker_name}.capitalize()": "{rate}""
+# 11. Also we need to add  get this some how {"total_paid": total_vault_cash, "log": audit_log}
 
-"""
+
 def audit_payroll(crew,hours,rate_db):
     if not isinstance(crew,list) or not isinstance(hours,list) or not isinstance(rate_db,dict):
         return "Error: Something went wrong"
