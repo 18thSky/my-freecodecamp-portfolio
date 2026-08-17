b043200575564533a983cb573d8aa12f6ea4bab6
18thSky <akashhajjarge@gmail.com>
2026-08-17 15:32:09 +0530

    Add new Python algorithm challenge

---
+++ py-algorithms/Budget App.py
@@ -0,0 +1,13 @@
+class Category:
+    def __init__(self,name):
+        self.name = name
+        self.ledger = []
+    def deposit(self,amount,description = ""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self,amount,description = ""):
+        self.ledger.append({"amount": -amount, "description": description})
+        return True
+
+def create_spend_chart(categories):
+    pass
