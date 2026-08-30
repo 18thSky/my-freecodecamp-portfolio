06160c62eb9f931088246eeaf475763f201dc7b3
18thSky <akashhajjarge@gmail.com>
2026-08-30 14:38:49 +0530

    Add new Python algorithm challenge

---
+++ py-algorithms/Player Interface.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0,0)
+        self.path = [(0,0)]
+
+    def make_move(self):
+        moves = [(0,1), (0,-1), (1,0), (-1,0)]
+        new_position = random.choice(self.moves)
+        self.position = new_position
+        self.path.append(new_position)
+        return self.position
+
+    
+    @abstractmethod
+    def level_up(self):
+        pass
+        
+class Pawn(Player):
+    def __init__(self):
+        super().__init__(self)
+        self.moves = moves
