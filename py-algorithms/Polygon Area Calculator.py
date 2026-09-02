1470e18665d5fd28ac3d5bafe5b7f4637d8887f3
18thSky <akashhajjarge@gmail.com>
2026-09-02 09:44:51 +0530

    Add new Python algorithm challenge

--- py-algorithms/Polygon Area Calculator.py
+++ py-algorithms/Polygon Area Calculator.py
@@ -21,18 +21,38 @@ class Rectangle():
         return m.sqrt((self.width*self.width)+(self.height*self.height))
     
     def __str__(self):
-        return f"{self.width},{self.height}"
+        return f"Rectangle(width={self.width}, height={self.height})"
 
     def get_picture(self):
         if self.width > 50 or self.height > 50:
             return f"Too big for picture."
-        title = self.width.center(5, "*")
-        output = title + "\n"
+        
+        line = "*" * self.width + "\n"
+        
+        final = line * self.height
+        return final
 
-    def get_amount_inside(self):
-        return 
+    def get_amount_inside(self,shape):
+        horizontal = self.width // shape.width
+        vertical = self.height // shape.height
+        return horizontal *vertical
 
 class Square(Rectangle):
-    def __init__(self):
-        super().__init__(width ,height)
+    def __init__(self,side):
+        super().__init__(side,side)
 
+    def __str__(self):
+        return f"Square(side={self.width})"
+
+    def set_width(self,width_value):
+        self.width = width_value
+        self.height = width_value
+
+    def set_height(self,height_value):
+        self.width = height_value
+        self.height = height_value
+
+    def set_side(self,side):
+        self.width = side
+        self.height = side
+        
