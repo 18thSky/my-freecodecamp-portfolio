429d74141a891c5ecd34e0dd51b40286a171c0b7
18thSky <akashhajjarge@gmail.com>
2026-09-09 10:57:35 +0530

    Add new Python algorithm challenge

--- py-algorithms/Hash Table.py
+++ py-algorithms/Hash Table.py
@@ -9,27 +9,44 @@ class HashTable():
         return total
         
     def add(self,key,value):
-       hashed = self.hash(key)
-       if hashed in self.collection:
-        self.collection[hashed] = {}
-        self.collection[hashed] [key] = value
+        hashed = self.hash(key)
 
-    
+        if hashed in self.collection:
+            self.collection[hashed][key] = value
+        else:
+            self.collection[hashed] = {}
+            self.collection[hashed][key] = value
+
+    def remove(self,key):
+        hashed = self.hash(key)
+
+        if hashed in self.collection:
+            if key in self.collection[hashed]:
+                del self.collection[hashed][key]
+
+    def lookup(self,key):
+        hashed = self.hash(key)
         
-
-
-    # def hash():
-    #     input(string)
-
-    # def add():
-
-    # def remove():
-
-    # def lookup():
+        if hashed in self.collection:
+            if key in self.collection[hashed]:
+                return self.collection[hashed][key]
+            else:
+                return None
 
 table = HashTable()
+
+print(table.lookup("golf"))
+
+table.add("golf", "sport")
+print(table.lookup("golf"))
+
+table.add("fcc", "coding")
+table.add("cfc", "chemical")
+
 print(table.collection)
 
-table = HashTable()
-print(table.hash("golf"))
+table.remove("fcc")
+print(table.collection)
 
+print(table.lookup("cfc"))
+print(table.lookup("fcc"))
